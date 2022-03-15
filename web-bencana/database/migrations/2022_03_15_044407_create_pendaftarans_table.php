17ccbb77dc641cee5ad617df26c3d98038da1116
PhanEdwardAdityaKurniawan <90235143+PhanEdwardAdityaKurniawan@users.noreply.github.com>
2022-03-15 16:02:14 +0700

    ndang digawe

---
+++ web-bencana/database/migrations/2022_03_15_044407_create_pendaftarans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePendaftaransTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pendaftarans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama')->nullable();
+            $table->string('lokasi')->nullable();
+            $table->string('jenisbencana')->nullable();
+            $table->string('deskripsi')->nullable();
+            $table->string('notelp')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pendaftarans');
+    }
+}
